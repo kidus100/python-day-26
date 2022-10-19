08e7ff9f7c8bb2bacdb70cad454be991b84f315d
kidus100 <kiyati.tech@gmail.com>
2022-10-19 17:33:09 -0400

    first commit

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import tkinter
 
 
 
-window = tkinter.TK()
+window = tkinter.Tk()
 
 window.title("My First GUI program")
+window.minsize(width=500, height=300)
+
+
+#Lable
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack(side="bottom")
